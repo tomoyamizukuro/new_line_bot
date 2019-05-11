80d9e098913c84c5fdfdfb069b753e78a2b719aa
Ah_Long <Ah_Long@users.noreply.github.com>
2019-05-11 13:43:11 +0900

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const server = require("express")();
+const line = require("@line/bot-sdk")();
+
+const line_config = {
+	channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+	channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+server.listen(process.env.PORT || 3000);
+
+server.post('/bot/webhook', line.middlewaere(line_config), (req, res, next) => {
+	res.sendStatus(200);
+	console.log(req.body);
+});
+	
