17cd32f5dc34e018beec49e07d2059adf9e91b33
Ah_Long <Ah_Long@users.noreply.github.com>
2019-05-11 15:35:18 +0900

    improving

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
+// -----------------------------------------------------------------------------
+// モジュールのインポート
 const server = require("express")();
-const line = require("@line/bot-sdk");
+const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
+// -----------------------------------------------------------------------------
+// パラメータ設定
 const line_config = {
-	channelAccessToken: process.env.LINE_ACCESS_TOKEN,
-	channelSecret: process.env.LINE_CHANNEL_SECRET,
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
 
+// -----------------------------------------------------------------------------
+// Webサーバー設定
 server.listen(process.env.PORT || 3000);
 
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-	res.sendStatus(200);
-	console.log(req.body);
-});
-	
+
+// APIコールのためのクライアントインスタンスを作成
 const bot = new line.Client(line_config);
 
-server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/webhook', line.middleware(line_config), (req, res, next) => {
+    // 先行してLINE側にステータスコード200でレスポンスする。
+    res.sendStatus(200);
 
-	res.sendStatus(200);
+    // すべてのイベント処理のプロミスを格納する配列。
+    let events_processed = [];
 
-	var events_processed = [];
+    // イベントオブジェクトを順次処理。
+    req.body.events.map((event) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            if (event.message.text == "こんにちは"){
+                // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                events_processed.push(bot.replyMessage(event.replyToken, {
+                    type: "text",
+                    text: "これはこれは"
+                }));
+            }
+        }
+    });
 
-	req.body.events.forEach((event) => {
-
-		if (event.type == 'message' && event.message.type == 'text'){
-			if (event.message.text == 'こんにちは'){
-				events_processed.push(bot.replyMessage(event.replyToken, {
-					type: "text",
-					text: "これはこれは"
-				}));
-			}
-		}
-	});
-
-
-	Promise.all(events_processed).then(
-		(response) => {
-			console.log(`${response.length} events(s) processed.`);
-		}
-	);
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
 });
